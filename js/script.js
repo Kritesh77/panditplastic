481f11a6928215e3dfdd2a7cdddf429033f1d902
Kritesh77 <55019533+Kritesh77@users.noreply.github.com>
2020-12-04 00:46:05 +0530

    yo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Hide Header on on scroll down
+$(document).ready(function() {
+    $(document).ready(function(){
+      $('.your-class').slick({
+        setting-name: setting-value
+      });
+    });
+});
