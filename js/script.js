8d8bfa5812d9068a7b7bf50da727d7ae94e25a51
Kritesh77 <55019533+Kritesh77@users.noreply.github.com>
2020-12-04 00:47:53 +0530

    slick trial

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Hide Header on on scroll down
 $(document).ready(function() {
     $(document).ready(function(){
-      $('.your-class').slick({
-        setting-name: setting-value
+      $('.slider').slick({
+        autoplay:false,
+        slidesToShow:4,
       });
     });
 });
